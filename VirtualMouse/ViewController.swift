9c09f88e235d2850f7bb5bb90c46c159e39590db
Matthew Anisovich <anisovich.matthew@gmail.com>
2021-11-02 19:56:01 +0300

    Initial Commit

---
+++ VirtualMouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualMouse
+//
+//  Created by exerhythm on 11/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
